df3a5bdb6059cf1cbd8590fe722a0fa67f0b369a
Néphélim <nephcohen@gmail.com>
2024-08-16 16:38:41 +0200

    soutenance 2

--- src/layout/Footer/index.jsx
+++ src/layout/Footer/index.jsx
@@ -1,9 +1,9 @@
 const Footer = () => {
-    return (
-        <footer>
-            <img src="/logo-footer.svg" className="logo" alt="logo officiel" />
-        <p>Kasa OC-11 © 2024 Youcodeuse</p>
-        </footer>
-    );
-    }
-    export default Footer;
+  return (
+    <footer>
+      <img src="/logo-footer.svg" className="logo" alt="logo officiel" />
+      <p>Kasa OC-11 © 2024 Youcodeuse</p>
+    </footer>
+  );
+};
+export default Footer;
