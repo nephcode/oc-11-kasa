01be3be5fe2b2d6875e790a94840dae95ab9a5a6
Néphélim <nephcohen@gmail.com>
2024-08-22 15:09:45 +0200

    Données Logement OK
    TODO
     - Mise en pages des components
     - Mise à pages du random null

--- src/pages/Properties/index.jsx
+++ src/pages/Properties/index.jsx
@@ -1,8 +1,9 @@
 // COMPONENTS IMPORT =============================== //
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 // DATA IMPORT ===================================== //
-import datas from "../../../public/data/logements.json";
+import data from "../../../public/data/logements.json";
 
 // STYLES IMPORT =================================== //
 import scss from "./property.module.scss";
@@ -10,20 +11,49 @@ import scss from "./property.module.scss";
 // REACT COMPONENT ================================= //
 const Property = () => {
   const { id } = useParams();
-  const elementData = datas.find(data => data.id === id);
-  console.table(elementData);
+  const [logement, setLogement] = useState(null);
 
-  const name = elementData.host.name.split(' ');
+  useEffect(() => {
+    const foundLogement = data.find((logement) => logement.id === id);
+    if (foundLogement) {
+      setLogement(foundLogement);
+    }
+  }, [id]);
 
+  if (!logement) {
+    return <div>Logement non trouvé</div>;
+  }
+
+  const { host, title, location, description, pictures, equipments, tags, rating } =
+    logement;
 
   return (
-    <main 
-      className={scss.pageProperty}      
-    >
-      <h1 className={scss.view}>{name}</h1>
-      <div className={scss.view}>Property {id}</div>
-
-      
+    <main className={scss.pageProperty}>
+      <h1 className={scss.view}>{title}</h1>
+      <div className={scss.host}>
+            <span>{host.name}</span>
+            <img src={host.picture} alt={title} />
+      </div>
+      <div className={scss.view}>{location}</div>
+      <div className={scss.view}>{description}</div>
+      <div className={scss.host}>
+        <ul>
+          {tags.map((tags, index) => (
+            <li key={index}>{tags}</li>
+          ))}
+        </ul>
+      </div>
+      <div className={scss.view}>{rating}</div>
+      <div>
+        {pictures.map((picture, index) => (
+          <img key={index} src={picture} alt={title} />
+        ))}
+      </div>
+      <div>
+        {equipments.map((equipment, index) => (
+          <div key={index}>{equipment}</div>
+        ))}
+      </div>
     </main>
   );
 };
